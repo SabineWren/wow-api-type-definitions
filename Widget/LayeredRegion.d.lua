0a000f517c3c60bd7f611409ab8bc485fc176988
SabineWren <sabinewrensc@gmail.com>
2024-08-06 22:37:44 -0700

    Added LayeredRegion

--- Widget/LayeredRegion.d.lua
+++ Widget/LayeredRegion.d.lua
@@ -1,5 +1,22 @@
 ---@meta
 
 --- [Open Documentation](https://wowpedia.fandom.com/wiki/Widget_API?oldid=278403#LayeredRegion)
+--- <br>Abstract type for objects that represent a screen rendering process, rather than a full blown frame.
 ---@class LayeredRegion: Region
 LayeredRegion = {}
+
+--- Get the Region's DrawLayer and sublayer within it
+---@return DrawLayer layer
+---@return nil|-8|-7|-6|-5|-4|-3|-2|-1|0|1|2|3|4|5|6|7 sublayer
+function LayeredRegion:GetDrawLayer() end
+
+--- Sets the draw layer for the Region
+---@param layer DrawLayer
+---@param sublayer? -8|-7|-6|-5|-4|-3|-2|-1|0|1|2|3|4|5|6|7 Defaults to 0 if not set.
+function LayeredRegion:SetDrawLayer(layer, sublayer) end
+
+---@param r number [0, 1]
+---@param g number [0, 1]
+---@param b number [0, 1]
+---@param a? number [0, 1]
+function LayeredRegion:SetVertexColor(r, g, b, a) end
