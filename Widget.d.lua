529f01a29e84239efd012a05e0424be0b55b2406
SabineWren <sabinewrensc@gmail.com>
2024-08-06 23:02:38 -0700

    Backdrop fields optional

--- Widget.d.lua
+++ Widget.d.lua
@@ -165,12 +165,12 @@
 ---@alias Insets { right: number, top: number, bottom: number, left: number }
 
 ---@class Backdrop
----@field bgFile string Texture path for background.
----@field edgeFile string Texture path for edges.
----@field tile boolean Tile if true else Stretch.
----@field tileSize number If tiling, size of each tiled copy of bgFile.
----@field edgeSize number Size of the edgefile. i.e. border thickness and corner size.
----@field insets Insets
+---@field bgFile? string Texture path for background.
+---@field edgeFile? string Texture path for edges.
+---@field tile? boolean Tile if true else Stretch.
+---@field tileSize? number If tiling, size of each tiled copy of bgFile.
+---@field edgeSize? number Size of the edgefile. i.e. border thickness and corner size.
+---@field insets? Insets
 
 -- ************************************************
 -- ************ Inheritance Hierarchy ************
