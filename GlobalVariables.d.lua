6d1fe4ed72e276211e36eafcfaa1302f4ee52feb
SabineWren <sabinewrensc@gmail.com>
2024-09-04 23:27:36 -0700

    Added ColorPickerFrame fields

--- GlobalVariables.d.lua
+++ GlobalVariables.d.lua
@@ -3,7 +3,16 @@
 BOOKTYPE_PET = "pet"
 BOOKTYPE_SPELL = "spell"
 
----@type ColorSelect
+---[Source](https://github.com/MOUZU/Blizzard-WoW-Interface/blob/master/1.12.1/FrameXML/ColorPickerFrame.xml)
+---[Documentation](https://wowpedia.fandom.com/wiki/Using_the_ColorPickerFrame?oldid=89462)
+---@class (exact) ColorPickerFrame : ColorSelect
+---@field cancelFunc fun(): nil
+---@field func fun(): nil
+---@field hasOpacity boolean
+---@field opacity number 0 to 1
+---@field previousValues [number, number, number] Values to apply on escape or close
+
+---@type ColorPickerFrame
 ColorPickerFrame = ColorPickerFrame
 
 ---@type ScrollingMessageFrame
